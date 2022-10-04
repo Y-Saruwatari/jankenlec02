30ef94d7b80219d09ac7373085d169c68e81c7d7
b20083 <e1b20083@st.oit.ac.jp>
2022-10-04 14:35:33 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/lec02/sanitao/jankenlec02/Jankenlec02Application.java
@@ -0,0 +1,13 @@
+package oit.is.lec02.sanitao.jankenlec02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jankenlec02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jankenlec02Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec02/sanitao/jankenlec02/Jankenlec02ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec02.sanitao.jankenlec02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jankenlec02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
